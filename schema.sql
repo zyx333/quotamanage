e01713175aa7995e8b8940270aff28ff80b2fece
zyx <zyxthr@gmail.com>
2024-01-30 22:34:11 +0800

    sql

--- schema.sql
+++ schema.sql
@@ -19,14 +19,14 @@ CREATE TABLE `user_account` (
      PRIMARY KEY (`id`), UNIQUE KEY `udx_user` (`user_id`) USING BTREE ) ENGINE=InnoDB
     AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='用户账号表';
 
-CREATE TABLE `user_account` (
+CREATE TABLE `quota_update_log` (
     `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增主键',
     `quota_account_id` bigint(20) NOT NULL COMMENT ' 额度账号 id',
     `update_type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '变更类型',
     `quota` double NOT NULL COMMENT '变更额度',
     `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
     `lastmodified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
-    PRIMARY KEY (`id`), UNIQUE KEY `udx_user` (`user_id`) USING BTREE ) ENGINE=InnoDB
-    AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='用户账号表';
+    PRIMARY KEY (`id`), KEY `idx_quota_account_id` (`quota_account_id`) USING BTREE ) ENGINE=InnoDB
+    AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='额度更新日志表';
 
 
