277f7ce659c0ec83ee20de2f1bda69dfc9cf3cc6
chixian <chixian@qunhemail.com>
2024-01-30 16:45:54 +0800

    transaction

--- src/main/java/com/quiz/quotamanage/DataSourceConfig.java
+++ src/main/java/com/quiz/quotamanage/DataSourceConfig.java
@@ -1,8 +1,24 @@
 package com.quiz.quotamanage;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        // TODO: 确认是否需要配置
+        return null;
+    }
+
+    @Bean("transactionManager")
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager();
+    }
 
 }
--- src/main/java/com/quiz/quotamanage/QuotaManageApplication.java
+++ src/main/java/com/quiz/quotamanage/QuotaManageApplication.java
@@ -4,9 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableTransactionManagement
 @MapperScan(basePackages = "com.quiz.quotamanage.mapper")
 public class QuotaManageApplication {
 
