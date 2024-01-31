cfb617f63fe8ee06495103aaa9cc5e07062ed051
chixian <chixian@qunhemail.com>
2024-01-31 15:23:06 +0800

    schedule task

--- src/main/java/com/quiz/quotamanage/service/QuotaScheduledService.java
+++ src/main/java/com/quiz/quotamanage/service/QuotaScheduledService.java
@@ -2,6 +2,7 @@ package com.quiz.quotamanage.service;
 
 import com.quiz.quotamanage.BizException;
 import com.quiz.quotamanage.data.QuotaAccountPo;
+import com.quiz.quotamanage.data.UserAccountDto;
 import com.quiz.quotamanage.mapper.QuotaAccountMapper;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -39,15 +40,74 @@ public class QuotaScheduledService {
 
     }
 
-    @Scheduled
+    @Scheduled(fixedDelay = 1000 * 60)
     public void initAccount() {
         Random random = new Random();
-        final long userId = random.nextLong();
+        final long userId = random.nextInt(1000);
         final int accountType = random.nextInt(3);
         try {
+            logger.info("initAccount, userId:{}, accountType:{}", userId, accountType);
             quotaAccountService.initAccount(userId, (byte) accountType);
         } catch (BizException e) {
-            throw new RuntimeException(e);
+            logger.error("initAccount failed. userId:{}, accountType:{}", userId, accountType, e);
+        }
+    }
+
+    @Scheduled(fixedDelay = 1000 * 80)
+    public void addQuotaAccount() {
+        Random random = new Random();
+        final long userId = random.nextInt(1000);
+        final int accountType = random.nextInt(3);
+        try {
+            logger.info("addQuotaAccount, userId:{}, accountType:{}", userId, accountType);
+            quotaAccountService.initAccount(userId, (byte) 0);
+
+            quotaAccountService.addQuotaAccount(userId, (byte) accountType);
+        } catch (BizException e) {
+            logger.error("addQuotaAccount failed. userId:{}, accountType:{}", userId, accountType, e);
+        }
+    }
+
+    @Scheduled(fixedDelay = 1000 * 100)
+    public void increaseQuota() {
+        Random random = new Random();
+        final long userId = random.nextInt(1000);
+        final int accountType = random.nextInt(3);
+        try {
+            logger.info("increaseQuota, userId:{}, accountType:{}", userId, accountType);
+            quotaAccountService.initAccount(userId, (byte) accountType);
+
+            quotaAccountService.increaseQuota(userId, (byte) accountType, 1000.0);
+        } catch (BizException e) {
+            logger.error("increaseQuota failed. userId:{}, accountType:{}", userId, accountType, e);
+        }
+    }
+
+    @Scheduled(fixedDelay = 1000 * 110)
+    public void decreaseQuota() {
+        Random random = new Random();
+        final long userId = random.nextInt(1000);
+        final int accountType = random.nextInt(3);
+        try {
+            logger.info("decreaseQuota, userId:{}, accountType:{}", userId, accountType);
+            quotaAccountService.initAccount(userId, (byte) accountType);
+
+            quotaAccountService.decreaseQuota(userId, (byte) accountType, -100.0);
+        } catch (BizException e) {
+            logger.error("decreaseQuota failed. userId:{}, accountType:{}", userId, accountType, e);
+        }
+    }
+
+    @Scheduled(cron = "0 0/1 * * * ?")
+    public void getQuotaAccountByUserAndType() {
+        Random random = new Random();
+        final long userId = random.nextInt(1000);
+        try {
+            logger.info("getQuotaAccountByUserAndType, userId:{}", userId);
+            final UserAccountDto userAccountDto = quotaAccountService.getQuotaAccountByUserAndType(userId);
+            logger.info("getQuotaAccountByUserAndType result:{}", userAccountDto);
+        } catch (Exception e) {
+            logger.error("getQuotaAccountByUserAndType failed. userId:{} ", userId, e);
         }
     }
 
